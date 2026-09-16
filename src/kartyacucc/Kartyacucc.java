aa5e7c24b94e71daea87932a78c6ae3a0875efa7
Ammer Márk Dániel (SZF_N_2025_2027) <ammer.mark.daniel@diak.szamalk-szalezi.hu>
2026-09-16 16:28:20 +0200

    first

---
+++ src/kartyacucc/Kartyacucc.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kartyacucc;
+
+/**
+ *
+ * @author AmmerMárkDániel(SZF_
+ */
+public class Kartyacucc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
