3ed9111a432e546a877c8eeb75cce80ca459bd54
Mark060705 <mhzcreosie@gmail.com>
2026-09-16 16:42:43 +0200

    second

--- src/kartyacucc/Kartyacucc.java
+++ src/kartyacucc/Kartyacucc.java
@@ -4,6 +4,8 @@
  */
 package kartyacucc;
 
+import java.util.Arrays;
+
 /**
  *
  * @author AmmerMárkDániel(SZF_
@@ -13,8 +15,47 @@ public class Kartyacucc {
     /**
      * @param args the command line arguments
      */
+    static String[] pakli = new String[22];
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        feltolt();
+        
+        System.out.println(Arrays.toString(pakli));
+        
+        for (int i = 0; i < 10; i++) {
+            kirak(); //1 tömb
+            melyik(); //scanner
+            kever(); //
+                    
+        }
+        ezVolt();
+
+                
     }
     
+    static void feltolt(){
+        String[] szinek = {"P", "T", "Z", "M"};
+        String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
+        int index = 1;
+
+        for (String szin : szinek) {
+            for (String ertek : ertekek) {
+
+                if (szin.equals("M") &&
+                    !(ertek.equals("Ász") ||
+                      ertek.equals("Kir") ||
+                      ertek.equals("Fel"))) {
+                    continue;
+                }
+
+                pakli[index] = szin + " " + ertek;
+                index++;
+            }
+        }
 }
+
+    
+}
+    
+
